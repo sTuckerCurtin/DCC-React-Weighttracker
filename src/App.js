315a9efa7a5b5eef7662ce098971aeadcf9bae4d
Samuel Curtin <stuckercurtin@gmail.com>
2023-05-11 17:12:24 -0400

    All but styling done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
+import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
 
 
 
@@ -7,9 +9,17 @@ function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"},{weight: 176, date: "11-24-2021"} ])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry]
+    setEntries(tempEntries)
+  }
+
+
   return (
     <div>
       <DisplayEntries parentEntries ={entries}/>
+      <AddEntryForm addNewEntry={addNewEntry}/>
+      <EntriesChartTracker parentEntries ={entries}/>
 
     </div>
   );
